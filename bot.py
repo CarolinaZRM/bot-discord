fa5fbad38326b770996c639d6beb7a1dcfe8ed8f
Fernando J. Bermúdez Medina <fernando.bermudez1@upr.edu>
2020-06-08 08:45:45 -0400

    Finally implemented nickname change in greeting

--- bot.py
+++ bot.py
@@ -32,33 +32,30 @@ async def on_message(message):
     else:
         print(f"""User: {message.author} tired to do command {message.content}, in channel {message.channel}""")
 
-# @client.event
-# async def on_member_update(before, after):
-#     nickname = after.nick
-#     if nickname:
-#         if nickname.lower().count("tim") > 0:
-#             last = before.nick
-#             if last:
-#                 await after.edit(nick=last)
-#             else:
-#                 await after.edit(nick="Invalid Nickname Change")
-
+@client.event
+async def on_member_update(before, after):
+    nickname = after.nick
+    if nickname:
+        if nickname.lower().count("tim") > 0:
+            last = before.nick
+            if last:
+                await after.edit(nick=last)
+            else:
+                await after.edit(nick="Invalid Nickname Change")
 
 
 @client.event
-async def on_member_join(member):
-        await member.send(f"""Welcome to the server {member.name}!""")
-        await member.send("Please enter your full name: ")
-        @client.event
-        async def on_message(msg):
-            await client.user.edit(nick=msg.content)
+async def on_member_join(member: discord.Member):
+    await member.send(f"""Welcome to the server {member.name}!""")
+    await member.send("Please enter your full name: ")
+
+    def check(m):  # checks if message was sent by someone other than the bot
+            return m.author != client.user
+
+    name = await client.wait_for("message",check=check)
+    await member.edit(nick=str(name.content))
 
 
 client.run(token)
 
-#every time a prepa joins the server, we ask the prepa for his name
-#this name will be stored in a map that will hold all of our prepa's info
-#map<K,V>; K=USERNAME, V=NAME
-#iterate thru every prepa in map as tuple:
-#    await msg.author.edit(nick=f"""{prepa.get_value()}""")
 
