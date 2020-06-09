f256fb07882a0dd5a885b4c5b9a03b03b32f4139
Fernando J. Bermúdez Medina <fernando.bermudez1@upr.edu>
2020-06-09 18:22:12 -0400

    Fixed some typos

--- bot.py
+++ bot.py
@@ -107,17 +107,17 @@ while True:
         
         #commands for admins and student counselors
         if str(message.channel).lower() in admin_channels and str(message.author) in VALIDATED_USERS:  
-            log('[DEBUG] Entered COunselor Auth Zone')
+            log('[DEBUG] Entered Counselor Auth Zone')
             if  "!user-count" in user_message.lower():
                 await message.channel.send(f"""Number of Members: {guild_id.member_count}""")
 
             if "!help" in user_message.lower():
                 embed = discord.Embed(title="Bot Commands for Counselors",description="Useful commands for counselors to ask the bot")
-                embed.add_field(name="/curriculo YOUR_DEPT", value="Gives the prepa the curriculum they request (INEL/ICOM/INSO/CIIC)")
-                embed.add_field(name="/map", value="Gives the prepa a map of UPRM")
-                embed.add_field(name="/links", value="Gives the prepa a PDF with all the important links of UPRM")
-                embed.add_field(name="/emails", value="Gives prepa a PDF with some important emails they can use")
-                embed.add_field(name="/office YOUR_DEPT", value="Tells the prepa what their dept office number is (INEL/ICOM or INSO/CIIC)")
+                embed.add_field(name="!curriculo YOUR_DEPT", value="Gives the user the curriculum they request (INEL/ICOM/INSO/CIIC)")
+                embed.add_field(name="!map", value="Gives the user a map of UPRM")
+                embed.add_field(name="!links", value="Gives the user a PDF with all the important links of UPRM")
+                embed.add_field(name="!emails", value="Gives user a PDF with some important emails they can use")
+                embed.add_field(name="!office YOUR_DEPT", value="Tells the user what their dept office number is (INEL/ICOM or INSO/CIIC)")
                 await message.author.send(content=None, embed=embed)
 
             if "!curriculo" in user_message.lower(): #Asked for curriculum
