d236f2b55b9ad667539f9ac5eb6fcbb3392d6987
Fernando J. Bermúdez Medina <fernando.bermudez1@upr.edu>
2020-06-11 21:52:43 -0400

    Made the contact embed a bit cleaner in code and reusable

--- handlers/telephone_guide.py
+++ handlers/telephone_guide.py
@@ -2,81 +2,68 @@ import discord
 from handlers.contacts import departamentos, servicios
 
 
+def generate_embed(contact, embed):
+    embed.add_field(name="Dept. Name", value=contact.contact_name)
+    embed.add_field(name="Dept. Description",
+                    value=contact.contact_description)
+    embed.add_field(name="Services Provided", value=contact.services_provided)
+    embed.add_field(name="Office", value=contact.office_number)
+
+    divisor = '\n\u2022 '
+    phone_list = f"\u2022 {divisor.join(contact.phone_number)}"
+    embed.add_field(name="Phones", value=phone_list)
+
+    extension_list = f"\u2022 {divisor.join(contact.extensions)}"
+    embed.add_field(name="Extension", value=extension_list)
+    embed.add_field(name="Work Hours", value=contact.work_hours)
+    embed.add_field(name="Google Maps Location", value=contact.gmaps_location)
+
+    return embed
+
+
 def get_dept_info(sections):
 
     if len(sections) <= 1:
         return 'Porfavor especifica abreviacion del departamento: INSO, ICOM, CIIC, INEL\n'\
             'Ejemplo: "!dept:inso"'
 
-    departartment_name: str = sections[1].lower()
+    department_name: str = sections[1].lower()
 
-    #return_message = f"Aqui esta la informacion de '{departartment_name.upper()}':\n\n"
     embed = None
-    if departartment_name in ('inso', 'ciic'):
+    if department_name in ('inso', 'ciic'):
         cse = departamentos.CSEDepartment()
         embed = discord.Embed(title="CSE Department Info",
                               description="Useful CSE department information")
-        embed.add_field(name="Dept. Name", value=cse.contact_name)
-        embed.add_field(name="Dept. Description",
-                        value=cse.contact_description)
-        embed.add_field(name="Services Provided", value=cse.services_provided)
-        embed.add_field(name="Office", value=cse.office_number)
 
-        divisor = '\n\u2022 '
-        phone_list = f"\u2022 {divisor.join(cse.phone_number)}"
+        return generate_embed(cse,embed)
 
-        embed.add_field(name="Phones", value=phone_list)
 
-        extension_list = f"\u2022 {divisor.join(cse.extensions)}"
-        embed.add_field(name="Extension", value=extension_list)
-
-        embed.add_field(name="Work Hours", value=cse.work_hours)
-        embed.add_field(name="Google Maps Location", value=cse.gmaps_location)
-
-        return embed
-        #return_message = f'{return_message}\n\n1. {departamentos.CSEDepartment()}'
-    elif departartment_name in ('icom', 'inel'):
-        #return_message = f'{return_message}1. {departamentos.ECEDepartment()}'
-
+    elif department_name in ('icom', 'inel'):
         ece = departamentos.ECEDepartment()
         embed = discord.Embed(title="ECE Department Info",
                               description="Useful ECE department information")
-        embed.add_field(name="Dept. Name", value=ece.contact_name)
-        embed.add_field(name="Dept. Description",
-                        value=ece.contact_description)
-        embed.add_field(name="Services Provided", value=ece.services_provided)
-        embed.add_field(name="Office", value=ece.office_number)
+        return generate_embed(ece,embed)
 
-        divisor = '\n\u2022 '
-        phone_list = f"\u2022 {divisor.join(ece.phone_number)}"
-        embed.add_field(name="Phones", value=phone_list)
 
-        extension_list = f"\u2022 {divisor.join(ece.extensions)}"
-        embed.add_field(name="Extension", value=extension_list)
-        embed.add_field(name="Work Hours", value=ece.work_hours)
-        embed.add_field(name="Google Maps Location", value=ece.gmaps_location)
-
-        return embed
     else:
         embed = "Departamento Invalido"
-        #return_message = f'Departamento invalido.'
 
     return embed
 
 
-def get_asistecia_econ(section):
+def get_asistencia_econ(section):
     return_message = "Aqui esta la informacion de la Oficina de Asistencia Economica:\n\n"
     return f'{return_message}1. {servicios.AsistenciaEconomica()}'
 
 
 def get_telephone_guide_help(sections):
-    telefone_list_keys = set()
+    telephone_list_keys = set()
     for key in _telephone_guide_list:
         if _telephone_guide_list[key]['func']:
             msg = f'{key}: {_telephone_guide_list[key]["description"]}'
-            telefone_list_keys.add(msg)
+            telephone_list_keys.add(msg)
     joiner = "\n\t\u2022 "
-    response = f'{_help_msg}\t\u2022 {joiner.join(telefone_list_keys)}'
+    response = f'{_help_msg}\t\u2022 {joiner.join(telephone_list_keys)}'
     return response
 
 
@@ -112,17 +99,16 @@ def get_faculty(sections):
         return 'Porfavor especifica abreviacion del departamento: INSO, ICOM, CIIC, INEL\n'\
             'Ejemplo: "!dept:inso"'
 
-    departartment_name: str = sections[1].lower()
+    department_name: str = sections[1].lower()
     embed = None
-    if departartment_name in ('inso', 'ciic'):
+    if department_name in ('inso', 'ciic'):
         embed = discord.Embed(title="CSE Faculty",
                               description="")
         embed.add_field(name="For more information visit this link", value="https://www.uprm.edu/cse/faculty/")
         for name, role in cse_faculty.items():
             embed.add_field(name=name, value=role)
 
-
-    elif departartment_name in ('inel', 'icom'):
+    elif department_name in ('inel', 'icom'):
         embed = discord.Embed(title="ECE Faculty",
                               description="")
 
@@ -135,18 +121,12 @@ def get_faculty(sections):
     return embed
 
 
-
-
-
-
-
-
 _telephone_guide_list = dict(
     {
         '!rectoria': {'func': None, 'description': 'Informacion de Contacto de Rectoria'},
         '!dept': {'func': get_dept_info, 'description': 'Obtener informacion de contacto de los departamentos de INEL/ICOM/INSO/CIIC'},
         '!contactos': {'func': get_telephone_guide_help, 'description': 'Obtener lista completa de contactos disponibles'},
-        '!aecon': {'func': get_asistecia_econ, 'description': 'Informacion de Contacto de Asistencia Economica'},
+        '!aecon': {'func': get_asistencia_econ, 'description': 'Informacion de Contacto de Asistencia Economica'},
         '!facultad': {'func': get_faculty, 'description': 'Obtener informacion de contacto de la facltad de los departamentos de INEL/ICOM/INSO/CIIC'},
 
     }
