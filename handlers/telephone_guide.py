fd2ae1910c934341b3751b827ea9b3c0f94a75af
Fernando J. Bermúdez Medina <fernando.bermudez1@upr.edu>
2020-06-11 21:01:35 -0400

    Finished Faculty Command

--- handlers/telephone_guide.py
+++ handlers/telephone_guide.py
@@ -66,7 +66,64 @@ def get_telephone_guide_help(sections):
     return response
 
 def get_faculty(sections):
-    pass
+    cse_faculty = {
+        "Bienvenido Velez Rivera": "Acting Dean of Engineering\nFull Time Professor\nbienvenido.velez@upr.edu",
+        "Pedro I. Rivera Vega": "Acting CSE Director\nFull Time Professor\np.rivera@upr.edu",
+        "Manuel Rodriguez Martinez": "Associate Director\nFull Time Professor\nmanuel.rodriguez7@upr.edu",
+        "Wilson Rivera Gallego": "Full Time Professor\nwilson.riveragallego@upr.edu",
+        "Kejie Lu": "Full Time Professor\nkejie.lu@upr.edu",
+        "Heidy Sierra Gil": "Associate Professor\nheidy.sierra1@upr.edu",
+        "Emmanuel Arzuaga Cruz": "Full Time Professor\nearzuaga@ece.uprm.edu",
+        "Marko Schütz Schmuck": "Full Time Professor\nmarko.schutz@upr.edu",
+        "Jose L. Melendez": "Special Assistant to the Chancellor\nFull Time Professor\njose.melendez37@upr.edu",
+        "Jaime Seguel": "Retired\njaime.seguel@upr.edu",
+        "Juan O. Lopez Gerena": "Instructor\njuano.lopez@upr.edu"
+    }
+
+    ece_faculty = {
+        "Gerson Beauchamp": "Full Time Professor\ngerson.beauchamp@upr.edu",
+        "Jaime Arbona Fazzi": "Full Time Professor\njaime.arbona@upr.edu",
+        "Jose Cedeño": "AFull Time Professor\njose.cedeno3@upr.edu",
+        "Isidoro Couvertier": "Full Time Professor\nisidoro.couvertiero@upr.edu",
+        "Shawn David Hunt": "Full Time Professor\nshawndavid.hunt@upr.edu",
+        "Henrick Ierick": "Full Time Professor\nhenrick.ierick@upr.edu",
+        "Rogelio Palomera ": "Full Time Professor\nrogelio.palomera@upr.edu",
+        "Manuel Jimenez": "Full Time Professor\nmanuel.jimenez@upr.edu",
+        "Nayda Santiago Santiago": "Full Time Professor\nnayda.santiago@upr.edu",
+        "Hamed Parsiani Gobadi": "Full Time Professor\nhamed.parsiani@upr.edu",
+        "Guillermo Serrano": "Full Time Professor\nguillermo.serrano.@upr.edu"
+    }
+    if len(sections) <= 1:
+        return 'Porfavor especifica abreviacion del departamento: INSO, ICOM, CIIC, INEL\n'\
+            'Ejemplo: "!dept:inso"'
+
+    departartment_name: str = sections[1].lower()
+    embed = None
+    if departartment_name in ('inso', 'ciic'):
+        embed = discord.Embed(title="CSE Faculty",
+                              description="")
+        embed.add_field(name="For more information visit this link", value="https://www.uprm.edu/cse/faculty/")
+        for name, role in cse_faculty.items():
+            embed.add_field(name=name, value=role)
+
+
+    elif departartment_name in ('inel', 'icom'):
+        embed = discord.Embed(title="ECE Faculty",
+                              description="")
+
+        embed.add_field(name="For more information visit this link", value="https://ece.uprm.edu/people/faculty/#cn-top")
+        for name, role in ece_faculty.items():
+            embed.add_field(name=name, value=role)
+
+    else:
+        embed = "Departamento Invalido"
+    return embed
+
+
+
+
+
+
 
 
 _telephone_guide_list = dict(
@@ -75,7 +132,7 @@ _telephone_guide_list = dict(
         '!dept': {'func': get_dept_info, 'description': 'Obtener informacion de contacto de los departamentos de INEL/ICOM/INSO/CIIC'},
         '!contactos': {'func': get_telephone_guide_help, 'description': 'Obtener lista completa de contactos disponibles'},
         '!aecon': {'func': get_asistecia_econ, 'description': 'Informacion de Contacto de Asistencia Economica'},
-        '!faculty': {'func': get_faculty, 'description': 'Obtener informacion de contacto de la facltad de los departamentos de INEL/ICOM/INSO/CIIC'},
+        '!facultad': {'func': get_faculty, 'description': 'Obtener informacion de contacto de la facltad de los departamentos de INEL/ICOM/INSO/CIIC'},
 
     }
 )
