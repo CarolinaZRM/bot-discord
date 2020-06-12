7090ccaf8a23cfaca01870f75a8a161ebbcbc005
Gabriel S Santiago <gabriel.santiago16@upr.edu>
2020-06-12 19:39:57 -0400

    Fixed bug when changing name in JOINING

--- event_handlers/join.py
+++ event_handlers/join.py
@@ -12,9 +12,12 @@ async def event_greet_new_member(client: discord.Client, member: discord.Member)
     )
 
     # checks if message was sent by the user and in the DM
-    def check_same_user(client_response):
-        log.debug(f'[DEBUG] c: {client_response} user: {member}')
-        return client_response.author == member
+    def check_same_user(client_response: discord.Message):
+        member_dm_id = member.dm_channel.id
+        log.debug(
+            f'[DEBUG - join.py | line.17] Client ResponseObj: "{client_response}" ||||| MemberObj: {member}')
+        # return true if message belongs to the same member and comes from DM
+        return client_response.author == member and client_response.channel.id == member_dm_id
 
     # Extracts the name of the student from the DM
     name = await client.wait_for("message", check=check_same_user)
@@ -22,7 +25,8 @@ async def event_greet_new_member(client: discord.Client, member: discord.Member)
     await member.send("**Gracias!**")
 
     # Replaces their old name to the one they provided in the DM to the bot
-    print(f"""{name.author}'s nickname was changed to {name.content}""")
+    log.debug(
+        f"[VERBOSE - join.py | line.20] {name.author}'s nickname was changed to {name.content}")
     await member.edit(nick=str(name.content))
     await member.send(
         f"Ya todos te veran como: '{name.content}'\n"
