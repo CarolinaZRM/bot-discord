c354d559372959be8f566b3bd0774c317f7f076a
Gabriel S Santiago <gabriel.santiago16@upr.edu>
2020-07-19 14:47:17 -0400

    Using utils.get to get roles

--- event_handlers/join.py
+++ event_handlers/join.py
@@ -4,6 +4,7 @@ import csv
 import os
 from typing import Dict
 import discord
+from discord import utils
 from discord.errors import Forbidden
 try:
     import log
@@ -126,11 +127,14 @@ async def assign_group(client: discord.Client, member: discord.Member, check_sam
 
     # el nombre del grupo esta en:
     # student_obj['group id'] o student_obj.get('group id')
-    student_role: str = student_obj['group id']
+    student_group: str = student_obj['group id']
     student_department: str = student_obj['department']
 
     try:
-        await member.add_roles(student_role.capitalize(), student_department.upper(), 'prepa')
+        group_role = utils.get(member.guild.roles, name=student_group)
+        dept_role = utils.get(member.guild.roles, name=student_department)
+        prepa_role = utils.get(member.guild.roles, name='prepa')
+        await member.add_roles(group_role, dept_role, prepa_role)
     except Forbidden:
         log.debug(
             '[ERROR] Bot does not have permision to add roles. Could not add student "{}" to group.'.format(student_obj))
