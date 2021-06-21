5119f37559b914b00e91e6613db35ed90ddd1bc4
Gabriel S Santiago <gabriel.santiago16@upr.edu>
2021-06-21 00:32:39 -0400

    Added constants for links

---
+++ constants/links.py
@@ -0,0 +1,10 @@
+'''
+//
+//  constants/links.py
+//  py-bot-uprm
+//
+//  Created by Gabriel Santiago on 06/20/2021.
+//  Copyright © 2021 bermedDev. All rights reserved.
+//  Copyright © 2021 teamMADE. All rights reserved.
+'''
+CURRICULO_CIIC_LINK = "https://www.uprm.edu/cse/bs-computer-science-and-engineering-2"
