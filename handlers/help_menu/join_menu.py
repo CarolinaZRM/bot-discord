58896df73efa8ac9eca25e7bca07f22e06be313f
Gabriel S Santiago <gabriel.santiago16@upr.edu>
2021-06-24 16:52:08 -0400

    Refactored /handler/help_menu from a single file into a module

---
+++ handlers/help_menu/join_menu.py
@@ -0,0 +1,15 @@
+'''
+//  /handlers/help_menu/join_menu.py
+//  py-bot-uprm
+//
+//  Created by Gabriel S. Santiago on 06/24/2021
+//  Copyright © 2021 bermedDev. All rights reserved.
+//  Copyright © 2021 agSant01. All rights reserved.
+//  Copyright © 2021 teamMADE. All rights reserved.
+'''
+
+import base_menu
+
+
+def help_menu_join():
+    return base_menu.help_menu_base()
