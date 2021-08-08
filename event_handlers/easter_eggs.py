5cb3d3e5bf501865e044027edf40877c73c5348d
Gabriel S Santiago <gabriel.santiago16@upr.edu>
2021-08-08 14:58:19 -0400

    Added TM song

--- event_handlers/easter_eggs.py
+++ event_handlers/easter_eggs.py
@@ -20,9 +20,10 @@ YET_REGEX = re.compile('y+ee+t+s*', re.IGNORECASE)
 
 
 async def subscribe_easter_eggs(message: discord.Message):
+    await _cancion_team_made(message)
+    await _do_parkour(message)
     await _is_sheeshed(message)
     await _is_yeeted(message)
-    await _do_parkour(message)
 
 
 async def _is_sheeshed(message: discord.Message):
@@ -56,15 +57,39 @@ PARKOUR_GIFS = ['https://tenor.com/view/parkour-theoffice-freerunning-gif-512824
 PARKOUR_RESPONSE = ['YAASSSS', 'Of course!!! :sunglasses:']
 
 
+def is_directed_to_bot(user_input):
+    # if does not start with "bot" or contains a BOT Tag, terminate
+    return re.match(r'^(bot).*', user_input) or re.search(r'(<@!719199208166522881>).*', user_input)
+
+
 async def _do_parkour(message: discord.Message):
-    CMD = r'(do)? *you *(do|know) *parkour *\?*'
+    CMD_ES = r' *(tu)? *haces *parkour *\?*'
+    CMD_EN = r'(do)? *you *(do|know) *parkour *\?*'
 
     user_input = message.content.lower()
 
     # if does not start with "bot" or contains a BOT Tag, terminate
-    if not (re.match(r'^(bot).*', user_input) or re.search(r'(<@!719199208166522881>).*', user_input)):
+    if not is_directed_to_bot(user_input):
         return
 
     # if contains the question, return GIF
-    if re.search(CMD, user_input):
+    if re.search(CMD_EN, user_input) or re.search(CMD_ES, user_input):
         await message.reply(content=f'{random.choice(PARKOUR_RESPONSE)}\n{random.choice(PARKOUR_GIFS)}')
+
+
+_TM_SONG_REGEX_ES = r'cu(a|á)l +es +(la +canci(o|ó)n|el himno) +(oficial )? *de +(tm|team made) *\?*'
+_TM_SONG_REGEX_EN = r'what +is +the +(off?icial)? *(song|anthem) *of +(tm|team made) *\?*'
+_TM_SONG_LINK = 'https://www.youtube.com/watch?v=21TfnF4m7os'
+
+
+async def _cancion_team_made(message: discord.Message):
+    user_input = message.content.lower()
+
+    if not is_directed_to_bot(user_input):
+        return
+
+    if re.search(_TM_SONG_REGEX_ES, user_input) or re.search(_TM_SONG_REGEX_EN, user_input):
+        await message.reply('La canción/himno de _Team MADE_ es **Grita por Jarabe de Palo**. '
+                            'El mensaje de esta canción es uno muy bonito y representa uno de nuestros pilares. '
+                            'El que siempre estaremos ahí el uno para el otro, sin importar la situacion. :relieved: :green_heart: '
+                            f'\n{_TM_SONG_LINK}')
