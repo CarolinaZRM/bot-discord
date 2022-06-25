6999a0ce31ce35102bbd5d81b76af305f083eee0
CarolinaZRM <carolina.rodriguez11@upr.edu>
2022-06-25 16:45:56 -0400

    main func - wsgi

--- main.py
+++ main.py
@@ -61,128 +61,130 @@ def handle_exit():
         except asyncio.CancelledError:
             pass
 
+def main():
+    global client
+    while True:
+        @client.event
+        async def on_message(message: discord.Message):
+            if (message.author.bot):
+                # Events related to bot response
+                return
 
-while True:
-    @client.event
-    async def on_message(message: discord.Message):
-        if (message.author.bot):
-            # Events related to bot response
-            return
+            log.debug(f'[INFO] [Func: on_message] MessageObj: {message}')
 
-        log.debug(f'[INFO] [Func: on_message] MessageObj: {message}')
+            adding_profanity = await sanitize.add_profanity_to_list(message)
 
-        adding_profanity = await sanitize.add_profanity_to_list(message)
+            if adding_profanity:
+                log.debug(f'[DEBUG] {message.author} added new profanity.')
+                return
 
-        if adding_profanity:
-            log.debug(f'[DEBUG] {message.author} added new profanity.')
-            return
+            has_profanity = await sanitize.profanity_filter(message)
+            if has_profanity:
+                log.debug('[DEBUG] Has profanity')
+                return
 
-        has_profanity = await sanitize.profanity_filter(message)
-        if has_profanity:
-            log.debug('[DEBUG] Has profanity')
-            return
-
-        log.debug(f"[USER] {message.author.name}#{message.author.id}")
-        if message.content.startswith("!bulk_delete_admin") \
-                and bot.is_sender_admin(message) \
-                and not bot.is_from_dm(message):
-            sections = message.content.split(':')
-            log.debug(f'[DEBUG] DELETE COMMAND SECTIONS: {sections}')
-            log.debug(
-                f"[DEBUG] BULK DELETE FILTER PASSED BY ADMIN {message.author}")
-            if len(sections) == 2 and sections[1].isdigit():
-                deleted = await message.channel.purge(limit=int(sections[1]))
+            log.debug(f"[USER] {message.author.name}#{message.author.id}")
+            if message.content.startswith("!bulk_delete_admin") \
+                    and bot.is_sender_admin(message) \
+                    and not bot.is_from_dm(message):
+                sections = message.content.split(':')
+                log.debug(f'[DEBUG] DELETE COMMAND SECTIONS: {sections}')
                 log.debug(
-                    f'[DEBUG] Deleted {len(deleted)} messages. " \
-                    " Selected by user {message.author}')
-            else:
-                while len(await message.channel.purge(limit=1500)) > 0:
+                    f"[DEBUG] BULK DELETE FILTER PASSED BY ADMIN {message.author}")
+                if len(sections) == 2 and sections[1].isdigit():
+                    deleted = await message.channel.purge(limit=int(sections[1]))
                     log.debug(
-                        f"[DEBUG DLT] BULK DELETE ' \
-                            'CALLED BY ADMIN {message.author}")
+                        f'[DEBUG] Deleted {len(deleted)} messages. " \
+                        " Selected by user {message.author}')
+                else:
+                    while len(await message.channel.purge(limit=1500)) > 0:
+                        log.debug(
+                            f"[DEBUG DLT] BULK DELETE ' \
+                                'CALLED BY ADMIN {message.author}")
 
-        log.debug('[INFO] passed the filter')
+            log.debug('[INFO] passed the filter')
 
-        # Created event passed Message object
-        # to use for response of bot to discord client
-        daily_logs.analytics(message)
-        await bot.set_streaming(client, message)
-        # await bot.join_voice_channel(client, message)
-        # await bot.leave_voice_channel(client, message)
-        # await bot.play_audio(client, message)
-        # await bot.pause_audio(client, message)
-        # await bot.resume_audio(client, message)
-        await actions.event_get_calendar(message)
-        await actions.event_get_curriculum(message)
-        await actions.event_get_freshman_guide(message)
-        await actions.event_help_menu(message)
-        await actions.event_parse_university_building(message)
-        await actions.event_telephone_guide(message)
-        await actions.event_uprm_map(message)
-        await actions.generate_faq(message)
-        await bot.general_leaderboard(message)
-        await actions.generate_server_rules(message)
-        await actions.get_org_info(message)
-        await actions.get_prj_info(message)
-        await attendance.subscribe_attendance(message)
-        await easter_eggs.subscribe_easter_eggs(message)
-        await fun_games.event_guessing_game(message, client)
-        await fun_games.event_ping_pong(message)
-        await fun_games.event_rock_paper_scissor(message, client)
-        await links.event_links(message)
-        await prepa.get_counselor_names(message)
-        await bot.level_on_message(message)
-        await bot.leveling_status(message)
-        await bot.download_user_level_data(message)
+            # Created event passed Message object
+            # to use for response of bot to discord client
+            daily_logs.analytics(message)
+            await bot.set_streaming(client, message)
+            # await bot.join_voice_channel(client, message)
+            # await bot.leave_voice_channel(client, message)
+            # await bot.play_audio(client, message)
+            # await bot.pause_audio(client, message)
+            # await bot.resume_audio(client, message)
+            await actions.event_get_calendar(message)
+            await actions.event_get_curriculum(message)
+            await actions.event_get_freshman_guide(message)
+            await actions.event_help_menu(message)
+            await actions.event_parse_university_building(message)
+            await actions.event_telephone_guide(message)
+            await actions.event_uprm_map(message)
+            await actions.generate_faq(message)
+            await bot.general_leaderboard(message)
+            await actions.generate_server_rules(message)
+            await actions.get_org_info(message)
+            await actions.get_prj_info(message)
+            await attendance.subscribe_attendance(message)
+            await easter_eggs.subscribe_easter_eggs(message)
+            await fun_games.event_guessing_game(message, client)
+            await fun_games.event_ping_pong(message)
+            await fun_games.event_rock_paper_scissor(message, client)
+            await links.event_links(message)
+            await prepa.get_counselor_names(message)
+            await bot.level_on_message(message)
+            await bot.leveling_status(message)
+            await bot.download_user_level_data(message)
 
-        # On message action for leveling system
+            # On message action for leveling system
 
-        if bot.is_sender_counselor(message):
-            # commands for admins and student counselors
-            log.debug('[DEBUG] Entered Counselor Auth Zone')
-            await channel.event_user_count(message)
-        elif bot.is_sender_prepa(message):
-            # commands for prepas
-            pass
+            if bot.is_sender_counselor(message):
+                # commands for admins and student counselors
+                log.debug('[DEBUG] Entered Counselor Auth Zone')
+                await channel.event_user_count(message)
+            elif bot.is_sender_prepa(message):
+                # commands for prepas
+                pass
 
-    @client.event
-    async def on_message_edit(before: discord.Message, after: discord.Message):
-        _ = await sanitize.profanity_filter(after)
+        @client.event
+        async def on_message_edit(before: discord.Message, after: discord.Message):
+            _ = await sanitize.profanity_filter(after)
 
-    @client.event
-    async def on_member_join(member: discord.Member):
-        await bot.verify_if_counselor(member)
-        await join.event_greet_new_member(client, member)
-        await join.made(member)
-        await bot.level_join(member)
+        @client.event
+        async def on_member_join(member: discord.Member):
+            await bot.verify_if_counselor(member)
+            await join.event_greet_new_member(client, member)
+            await join.made(member)
+            await bot.level_join(member)
 
-    @client.event
-    async def on_member_update(before, after):
-        if before.roles != after.roles:
-            log.debug(f'[DEBUG] before: {before} After: {after}')
+        @client.event
+        async def on_member_update(before, after):
+            if before.roles != after.roles:
+                log.debug(f'[DEBUG] before: {before} After: {after}')
+                await bot.update_admin_list(client)
+                prepa.extract_counselors(client)
+
+        @client.event
+        async def on_ready():
+            log.debug(f'[DEBUG] Guild Obj: {client.guilds}')
             await bot.update_admin_list(client)
             prepa.extract_counselors(client)
+            log.debug('[VERBOSE] On Ready Finished.')
 
-    @client.event
-    async def on_ready():
-        log.debug(f'[DEBUG] Guild Obj: {client.guilds}')
-        await bot.update_admin_list(client)
-        prepa.extract_counselors(client)
-        log.debug('[VERBOSE] On Ready Finished.')
+        client.loop.create_task(task())
+        # THIS IS WHERE IT BEGINS
+        try:
+            client.loop.run_until_complete(
+                client.start(config.BOT_TOKEN)
+            )
+        except SystemExit as e:
+            log.debug(f'[DEBUG] Error {e}')
+            handle_exit()
+        except KeyboardInterrupt:
+            handle_exit()
+            client.loop.close()
+            log.debug("[DEBUG] Program ended")
+            break
 
-    client.loop.create_task(task())
-    try:
-        client.loop.run_until_complete(
-            client.start(config.BOT_TOKEN)
-        )
-    except SystemExit as e:
-        log.debug(f'[DEBUG] Error {e}')
-        handle_exit()
-    except KeyboardInterrupt:
-        handle_exit()
-        client.loop.close()
-        log.debug("[DEBUG] Program ended")
-        break
-
-    log.debug("[DEBUG] Bot restarting")
-    client = discord.Client(loop=client.loop)
+        log.debug("[DEBUG] Bot restarting")
+        client = discord.Client(loop=client.loop)
---
+++ wsgi.py
@@ -0,0 +1,6 @@
+#in wsgi.py
+
+from main import main
+
+if __name__ == "__main__":
+    main()
