9cb5e0e448c90ed47550623879b919393a78bdb6
Gabriel S Santiago <gabriel.santiago16@upr.edu>
2022-08-09 11:11:27 -0400

    Updated attendance script

--- attendance.py
+++ attendance.py
@@ -17,52 +17,68 @@ enabled_intents.guilds = True
 #  - CHANGE IF NECESSARY
 #  - CURRENTLY POITING TO: prepas_conferenceRoom
 ###
-CHANNEL_ID = 849684994641494053
+CHANNEL_ID = 989729348099645463
+
+
+NOT_ASSIGNED = "NOT_ASSIGNED"
 
 
 class AssistanceClient(discord.Client):
     async def on_ready(self):
-        print('Logged on as {0}!'.format(self.user))
-        print('*' * 30)
+        print("Logged on as {0}!".format(self.user))
+        print("*" * 30)
 
         guild: discord.Guild = self.get_guild(config.GUILD_ID_NUM)
         channel = guild.get_channel(CHANNEL_ID)
 
-        print('=> Obteniendo miembros conectados...')
+        print("=> Obteniendo miembros conectados...")
 
-        members_in_attendance = [(idx + 1, member.nick if member.nick else member.name, '|'.join(filter(lambda role: role != '@everyone', [role.name for role in member.roles])))
-                                 for idx, member in enumerate(channel.members)]
+        members_in_attendance = [
+            (
+                idx + 1,
+                member.nick if member.nick else member.name,
+                "|".join(
+                    filter(
+                        lambda role: role != "@everyone",
+                        [role.name for role in member.roles],
+                    )
+                ),
+            )
+            for idx, member in enumerate(channel.members)
+        ]
 
-        print('=> Lista de miembros descargada...')
+        print("=> Lista de miembros descargada...")
 
         now = datetime.now()  # current date and time
-        formatted_datetime = now.strftime('%Y%m%d%H%M%S')
+        formatted_datetime = now.strftime("%Y%m%d%H%M%S")
 
-        list_file_path = self.attendance_list(
-            members_in_attendance, formatted_datetime)
+        list_file_path = self.attendance_list(members_in_attendance, formatted_datetime)
         stats_file_path = self.attendance_stats(
-            members_in_attendance, formatted_datetime)
+            members_in_attendance, formatted_datetime
+        )
 
         print()
         print(
-            f'=> Lista de miembros en el canal "{channel}" fue exitosamente guardada en el archivo:')
-        print(f'=> {path.abspath(list_file_path)}')
+            f'=> Lista de miembros en el canal "{channel}" fue exitosamente guardada en el archivo:'
+        )
+        print(f"=> {path.abspath(list_file_path)}")
 
         print()
         print(
-            f'=> Estadisticas de miembros en el canal "{channel}" fue exitosamente guardada en el archivo:')
-        print(f'=> {path.abspath(stats_file_path)}')
-        print('*' * 30)
+            f'=> Estadisticas de miembros en el canal "{channel}" fue exitosamente guardada en el archivo:'
+        )
+        print(f"=> {path.abspath(stats_file_path)}")
+        print("*" * 30)
 
     def attendance_list(self, members_in_attendance, formatted_datetime):
-        file_name = f'attendance-{formatted_datetime}.csv'
+        file_name = f"attendance-{formatted_datetime}.csv"
 
-        with open(file_name, 'w') as attendance_file:
+        with open(file_name, "w") as attendance_file:
             #  Create instance of CSV Writer
             wtr = csv.writer(attendance_file)
 
             # Create csv header
-            wtr.writerow(['ID', 'Nickname/Account Username', 'Lista de roles'])
+            wtr.writerow(["ID", "Nickname/Account Username", "Lista de roles"])
 
             #  Save members list to CSV
             for connected_member in members_in_attendance:
@@ -71,28 +87,35 @@ class AssistanceClient(discord.Client):
         return path.abspath(file_name)
 
     def attendance_stats(self, members_in_attendance, formatted_datetime):
-        file_name = f'attendance-stats-{formatted_datetime}.csv'
+        file_name = f"attendance-stats-{formatted_datetime}.csv"
 
         member_roles_count = collections.defaultdict(int)
 
         for member in members_in_attendance:
             # in position 3 of the members list is the roles list separated by "|"
             if len(member[2]) == 0:
-                member_roles_count['No assigned yet'] += 1
+                member_roles_count[NOT_ASSIGNED] += 1
                 continue
 
-            member_roles = member[2].split('|')
+            member_roles = member[2].split("|")
 
             for role in member_roles:
                 member_roles_count[role] += 1
 
         # save into stats file
-        with open(file_name, 'w') as stats_file:
+        with open(file_name, "w") as stats_file:
             writer = csv.writer(stats_file)
-            writer.writerow(
-                ['ID', 'Role Name', 'Quantity of members with role'])
+            writer.writerow(["ID", "Role Name", "Quantity of members with role"])
             for idx, item in enumerate(sorted(member_roles_count.items())):
                 writer.writerow([idx + 1, item[0], item[1]])
+            writer.writerow([idx + 2, "Total Miembros", len(members_in_attendance)])
+            writer.writerow(
+                [
+                    idx + 3,
+                    "Prepa + NOT_ASSIGNED",
+                    member_roles_count["prepa"] + member_roles_count[NOT_ASSIGNED],
+                ]
+            )
 
         return path.abspath(file_name)
 
